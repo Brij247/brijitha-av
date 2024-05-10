2674f6d2dc8e235cca4de80c2e124ff1cc764289
Brij247 <brijithaav@gmail.com>
2024-05-10 21:11:59 +0530

    feat(hero-layout): structure hero page with content and minr styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,99 @@
 import "./App.css";
 
+import {
+  EnvelopeOpenIcon,
+  GitHubLogoIcon,
+  LinkedInLogoIcon,
+  RocketIcon,
+  SunIcon,
+} from "@radix-ui/react-icons";
+
 function App() {
   return (
     <>
-      <h1>👩‍💻</h1>
-      <h1>Brijitha AV</h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "end",
+          alignItems: "center",
+          gap: "10px",
+        }}
+      >
+        <button>
+          <GitHubLogoIcon />
+        </button>
+        <button>
+          <SunIcon />
+        </button>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "20px",
+          padding: "8px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "start",
+            alignItems: "start",
+          }}
+        >
+          <h1>Brijitha AV</h1>
+          <h3>Frontend engineer</h3>
+          <p
+            style={{
+              textAlign: "left",
+            }}
+          >
+            I'm{" "}
+            <span
+              style={{
+                fontWeight: "bold",
+              }}
+            >
+              Brijitha
+            </span>
+            , your friendly neighborhood front-end enthusiast with a passion for
+            crafting captivating digital experiences. I enjoy building web
+            applications, creating videos and gardening. Currently, I'm building{" "}
+            <span
+              style={{
+                fontWeight: "bold",
+              }}
+            >
+              {" "}
+              Biblio-traker
+            </span>
+            , an open-source plateform built with{" "}
+            <span
+              style={{
+                fontWeight: "bold",
+              }}
+            >
+              Next.js{" "}
+            </span>
+            features!💻✨
+          </p>
+          <button>
+            Hire me{"  "}
+            <RocketIcon />
+          </button>
+          <div>
+            <button>
+              <LinkedInLogoIcon />
+            </button>
+            <button>
+              <EnvelopeOpenIcon />
+            </button>
+          </div>
+        </div>
+        <img src={"./src/briji.png"} />
+      </div>
     </>
   );
 }
